0a4d09d19dd0c7ae82fd1133be1e1eb3cae67139
Cylunny <102215486+Cylunny@users.noreply.github.com>
2023-11-01 13:56:12 +0100

    show cultivation item tag info on item view

--- resources/views/world/_item_entry.blade.php
+++ resources/views/world/_item_entry.blade.php
@@ -39,6 +39,23 @@
                 </div>
             </div>
         </div>
+        <div class="row">
+            @if($item->seedRewards())
+                <div class="col-md">
+                    <p><strong>Cultivates:</strong> @foreach($item->seedRewards() as $reward) {{ $reward['quantity'] }}x {!! $reward['asset']->displayName !!} @endforeach</p>
+                </div>
+            @endif
+            @if($item->toolPlot())
+                <div class="col-md">
+                    <p><strong>Creates Plot:</strong> {{ $item->toolPlot()->name }}</p>
+                </div>
+            @endif
+            @if($item->explorationArea())
+                <div class="col-md">
+                    <p><strong>Unlocks Area:</strong> {{ $item->explorationArea()->name }}</p>
+                </div>
+            @endif
+        </div>
         <div class="world-entry-text">
             @if(isset($item->reference) && $item->reference && Config::get('lorekeeper.extensions.item_entry_expansion.extra_fields'))  <p><strong>Reference Link:</strong> <a href="{{ $item->reference }}">{{ $item->reference }}</a></p> @endif
             {!! $description !!}
