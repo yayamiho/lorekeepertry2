73e17ea323b461f123140bce58f88946e460770f
perappu <perappu@users.noreply.github.com>
2024-06-14 17:35:07 +0000

    refactor: fix blade formatting

--- resources/views/admin/carousel/index.blade.php
+++ resources/views/admin/carousel/index.blade.php
@@ -1,81 +1,81 @@
 @extends('admin.layout')
 
 @section('admin-title')
-File Manager
+    File Manager
 @endsection
 
 @section('admin-content')
-{!! breadcrumbs(['Admin Panel' => 'admin', 'Carousel' => 'admin/data/carousel']) !!}
+    {!! breadcrumbs(['Admin Panel' => 'admin', 'Carousel' => 'admin/data/carousel']) !!}
 
-<h1>Carousel Manager</h1>
+    <h1>Carousel Manager</h1>
 
-<p>This page allows you to upload carousel images for the front page.</p>
+    <p>This page allows you to upload carousel images for the front page.</p>
 
-{!! Form::open(['url' => 'admin/data/carousel/create', 'files' => true]) !!}
+    {!! Form::open(['url' => 'admin/data/carousel/create', 'files' => true]) !!}
 
-<div class="p-4">
-    <div class="form-group">
-        {!! Form::label('link') !!}
-        {!! Form::text('link', '', ['class' => 'form-control']) !!}
+    <div class="p-4">
+        <div class="form-group">
+            {!! Form::label('link') !!}
+            {!! Form::text('link', '', ['class' => 'form-control']) !!}
+        </div>
+
+        <div class="form-group">
+            {!! Form::label('alt text') !!} {!! add_help('This is for accessibility purposes.') !!}
+            {!! Form::text('alt_text', '', ['class' => 'form-control']) !!}
+        </div>
+
+        <div class="form-group">
+            {!! Form::label('Image') !!}
+            <div>{!! Form::file('image') !!}</div>
+        </div>
+
+        <div class="text-right">
+            {!! Form::submit('Create', ['class' => 'btn btn-primary']) !!}
+        </div>
+
+        {!! Form::close() !!}
     </div>
 
-    <div class="form-group">
-        {!! Form::label('alt text') !!} {!! add_help('This is for accessibility purposes.') !!}
-        {!! Form::text('alt_text', '', ['class' => 'form-control']) !!}
-    </div>
-
-    <div class="form-group">
-        {!! Form::label('Image') !!}
-        <div>{!! Form::file('image') !!}</div>
-    </div>
-
-    <div class="text-right">
-        {!! Form::submit('Create', ['class' => 'btn btn-primary']) !!}
-    </div>
-
-    {!! Form::close() !!}
-    </div>
-
-<table class="table table-sm">
-    <thead>
-        <tr>
-            <th>Image</th>
-            <th>Link</th>
-            <th>Alt Text</th>
-            <th></th>
-        </tr>
-    </thead>
-    <tbody>
-        @foreach ($carousels as $carousel)
-        <tr>
-            <td>
-                <a href="">{{ $carousel->image }}</a>
-            </td>
-            <td>
-                <a href="">{{ $carousel->link }}</a>
-            </td>
-            <td>
-                <a href="">{{ $carousel->alt_text }}</a>
-            </td>
-            <td class="text-right">
-                <a href="#" class="btn btn-outline-danger btn-sm delete-carousel" data-name="{{ $carousel }}">Delete</a>
-            </td>
-        </tr>
-        @endforeach
-    </tbody>
-</table>
+    <table class="table table-sm">
+        <thead>
+            <tr>
+                <th>Image</th>
+                <th>Link</th>
+                <th>Alt Text</th>
+                <th></th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach ($carousels as $carousel)
+                <tr>
+                    <td>
+                        <a href="">{{ $carousel->image }}</a>
+                    </td>
+                    <td>
+                        <a href="">{{ $carousel->link }}</a>
+                    </td>
+                    <td>
+                        <a href="">{{ $carousel->alt_text }}</a>
+                    </td>
+                    <td class="text-right">
+                        <a href="#" class="btn btn-outline-danger btn-sm delete-carousel" data-name="{{ $carousel }}">Delete</a>
+                    </td>
+                </tr>
+            @endforeach
+        </tbody>
+    </table>
 @endsection
 
 @section('scripts')
     @parent
-    @if(isset($carousel))
-                <script>
-                    $(document).ready(function() {
-                        $('.delete-carousel').on('click', function(e) {
-                            e.preventDefault();
-                            loadModal("{{ url('admin/data/carousel/delete') }}/{{ $carousel->id }}", 'Delete Carousel');
-                        });
-                    });
-                </script>
+    @if (isset($carousel))
+        <script>
+            $(document).ready(function() {
+                $('.delete-carousel').on('click', function(e) {
+                    e.preventDefault();
+                    loadModal("{{ url('admin/data/carousel/delete') }}/{{ $carousel->id }}", 'Delete Carousel');
+                });
+            });
+        </script>
     @endif
 @endsection
--- resources/views/widgets/_carousel.blade.php
+++ resources/views/widgets/_carousel.blade.php
@@ -1,19 +1,19 @@
 <div id="carouselDashboard" class="carousel slide" data-ride="carousel" data-interval="20000">
-  <div class="carousel-inner">
-    @foreach (\App\Models\Carousel\Carousel::all() as $carousel)
-        @if ($loop->first)
-            <div class="carousel-item active"><a href="{{ $carousel->link }}"><img class="d-block w-100" src="{{ $carousel->imageURL }}" alt="{{ $carousel->alt_text }}"></a></div>
-        @else
-            <div class="carousel-item"><a href="{{ $carousel->link }}"><img class="d-block w-100" src="{{ $carousel->imageURL }}" alt="{{ $carousel->alt_text }}"></a></div>
-        @endif
-    @endforeach
-  </div>
-  <a class="carousel-control-prev" href="#carouselDashboard" role="button" data-slide="prev">
-    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-    <span class="sr-only">Previous</span>
-  </a>
-  <a class="carousel-control-next" href="#carouselDashboard" role="button" data-slide="next">
-    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-    <span class="sr-only">Next</span>
-  </a>
+    <div class="carousel-inner">
+        @foreach (\App\Models\Carousel\Carousel::all() as $carousel)
+            @if ($loop->first)
+                <div class="carousel-item active"><a href="{{ $carousel->link }}"><img class="d-block w-100" src="{{ $carousel->imageURL }}" alt="{{ $carousel->alt_text }}"></a></div>
+            @else
+                <div class="carousel-item"><a href="{{ $carousel->link }}"><img class="d-block w-100" src="{{ $carousel->imageURL }}" alt="{{ $carousel->alt_text }}"></a></div>
+            @endif
+        @endforeach
+    </div>
+    <a class="carousel-control-prev" href="#carouselDashboard" role="button" data-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="sr-only">Previous</span>
+    </a>
+    <a class="carousel-control-next" href="#carouselDashboard" role="button" data-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="sr-only">Next</span>
+    </a>
 </div>
