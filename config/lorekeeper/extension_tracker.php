5e7958395218b3ab33c54a137b9d2cb9762e3866
Cylunny <cydraws@gmail.com>
2024-07-22 07:48:32 +0200

    update extension tracker

--- config/lorekeeper/extension_tracker.php
+++ config/lorekeeper/extension_tracker.php
@@ -12,5 +12,13 @@ return [
     //     'version' => '1.0.0',
     // ],
     
+    'alternate_site_designs' => [
+        'key' => 'alternate_site_designs',
+        'wiki_key' => 'Alternate Site Designs',
+        'creators' => json_encode([
+            'Cylunny' => 'https://toyhou.se/cylunny',
+        ]),
+        'version' => '1.0.0', 
+    ],
 
 ];
