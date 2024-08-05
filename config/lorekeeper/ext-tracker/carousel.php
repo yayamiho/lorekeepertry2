7cf5eaff9af4e790bf73412aab2a3dff2f73f34c
Kazimir <stokoriel@gmail.com>
2024-08-05 17:56:11 -0500

    fix name on ext tracker

--- config/lorekeeper/ext-tracker/carousel.php
+++ config/lorekeeper/ext-tracker/carousel.php
@@ -3,7 +3,7 @@
 return [
     'wiki_key' => 'Carousel',
     'creators' => json_encode([
-        'Uri' => 'https://github.com/perappu',
+        'Stokori' => 'https://github.com/perappu',
     ]),
     'version'  => '3.0.0',
 ];
